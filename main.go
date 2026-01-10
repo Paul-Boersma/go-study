bde288ae9b4d74dce3c749e7edf3d93f9a8e1793
Paul Boersma <pcboersma91@gmail.com>
2026-01-10 11:39:02 +0100

    day 4

--- main.go
+++ main.go
@@ -1,82 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
 
 func main() {
-	operation, err := requestOperation()
+	// Exercise day 4
+
+	// print usefull statistics about the numbers
+	var data data
+
+	input, err := askUserInput()
 	if err != nil {
+		fmt.Println(err)
 		return
 	}
 
-	input1, input2, err := readValuesInput()
-	if err != nil {
-		return
+	data.Count = len(input)
+	for _, value := range input {
+		data.Min = math.Min(data.Min, value)
+		data.Max = math.Max(data.Max, value)
+		data.Sum += value
 	}
+	data.Avg = data.Sum / float64(data.Count)
 
-	result, err := performOperation(operation, input1, input2)
-	if err != nil {
-		fmt.Printf("Error performing calculation: %v", err)
-		return
-	}
-
-	fmt.Printf("Calculation result: %v", result)
+	fmt.Printf(`Data:
+Count: %v
+Min: %v
+Max: %v
+Sum: %v
+Avg: %.2f
+`, data.Count, data.Min, data.Max, data.Sum, data.Avg)
 }
 
-func requestOperation() (input int, err error) {
-	fmt.Println(`Choose operation:
-	1) Add
-	2) Subtract
-	3) Multiply
-	4) Divide`)
+func askUserInput() ([]float64, error) {
+	var userInput []float64
+	var input string
 
-	_, err = fmt.Scanf("%d\n", &input)
-	if err != nil || input <= 0 || input > 4 {
-		return 0, err
-	}
-	return input, nil
-}
+	fmt.Println("Enter digits. When ready type 'done'")
+	for {
+		fmt.Scanf("%v\n", &input)
+		if input == "done" {
+			return userInput, nil
+		}
 
-func readValuesInput() (input1 float64, input2 float64, err error) {
-	fmt.Println("Provide numbers you'd like to use in the following format: %f %f")
-	_, err = fmt.Scanf("%f %f", &input1, &input2)
-	if err != nil {
-		return
-	}
+		formattedInput, err := strconv.ParseFloat(input, 64)
+		if err != nil {
+			fmt.Println("input not valid, please enter digits or type 'done' to exit")
+			continue
+		}
 
-	return input1, input2, nil
-}
-
-func performOperation(operation int, input1 float64, input2 float64) (float64, error) {
-	switch operation {
-	case 1:
-		return add(input1, input2), nil
-	case 2:
-		return sub(input1, input2), nil
-	case 3:
-		return multiply(input1, input2), nil
-	case 4:
-		return divide(input1, input2)
-	default:
-		return 0, fmt.Errorf("Invalid operation choice")
+		userInput = append(userInput, formattedInput)
 	}
 }
 
-func add(a float64, b float64) float64 {
-	return a + b
-}
-
-func sub(a float64, b float64) float64 {
-	return a - b
-}
-
-func multiply(a float64, b float64) float64 {
-	return a * b
-}
-
-// How to optionally return?
-func divide(a float64, b float64) (float64, error) {
-	if b == 0 {
-		return 0, fmt.Errorf("Cannot divide by 0.")
-	}
-	return (a / b), nil
+type data struct {
+	Count int
+	Sum   float64
+	Avg   float64
+	Min   float64
+	Max   float64
 }
